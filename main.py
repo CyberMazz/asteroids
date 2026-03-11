72aa14f7e9510884f9192a2344836843f0ea6340
CyberMazz <mahzenbabekir@gmail.com>
2026-03-11 15:49:27 -0400

    created base gamestate

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pygame
+from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
