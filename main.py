4eec9257ce19e25333537691e8f34c9d5abd01d4
CyberMazz <mahzenbabekir@gmail.com>
2026-03-11 18:22:45 -0400

    fixing the 'x' button

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
-        for event in pygame.event.get():
-            pass
-        screen.fill("black")
-        pygame.display.flip()
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
