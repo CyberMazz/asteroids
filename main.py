a83e467541288578b1a40b507cca4de8ef89b410
CyberMazz <mahzenbabekir@gmail.com>
2026-03-15 19:26:28 -0400

    added asteroids and basic collision physics

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import pygame
-from logger import log_state
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+import sys
+from logger import log_state, log_event
+from constants import *
 from player import Player
+from asteroidfield import AsteroidField
+from asteroids import Asteroid
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,8 +26,14 @@ def main():
                 pygame.quit()
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Player hit an asteroid! Game over!")
+                sys.exit()
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
