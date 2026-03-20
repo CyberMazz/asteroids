10fd13d8e6641743d3da8e917c7ec5ddaedfef8a
CyberMazz <mahzenbabekir@gmail.com>
2026-03-20 18:05:23 -0400

    created shooting mechanics and added a shot delay

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from constants import *
 from player import Player
 from asteroidfield import AsteroidField
 from asteroids import Asteroid
+from shot import Shot
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -14,8 +15,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     AsteroidField.containers = (updatable)
     Asteroid.containers = (updatable, drawable, asteroids)
+    Shot.containers = (updatable, drawable, shots)
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
